827a03fc4b2450d24eeb9e32831bab87c1070260
zhiruchen <zhiruchen1992@foxmail.com>
2017-04-06 23:32:11 +0800

    add parsers

--- imp_parser.py
+++ imp_parser.py
@@ -29,7 +29,120 @@ class Reserved(Parser):
         self.tag = tag
 
     def __call__(self, tokens, pos):
-        if pos < len(tokens) and tokens[pos][0] == self.value and tokens[pos][1] == self.tag:
+        if pos < len(tokens) and tokens[pos][0] == self.value and tokens[pos][1] is self.tag:
             return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+
+class Tag(Parser):
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __call__(self, tokens, pos):
+        if pos < len(tokens) and tokens[pos][1] is self.tag:
+            return Result(tokens[pos][0], pos + 1)
+        else:
+            return None
+
+
+class Concat(Parser):
+    """the Concat combinator take two parser as input
+    first apply left parser, followed by right parser.
+    """
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            right_result = self.right(tokens, pos)
+            if right_result:
+                combined_value = (left_result.value, right_result.value)
+                return Result(combined_value, right_result.pos)
+
+            return None
+
+
+class Alternate(Parser):
+    """应用左边的parser, 如果失败应用右边的Parser返回它的结果
+    Alternate is useful for choosing among several possible parsers
+    """
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __call__(self, tokens, pos):
+        left_result = self.left(tokens, pos)
+        if left_result:
+            return left_result
+        else:
+            right_result = self.right(tokens, pos)
+            return right_result
+
+
+class Opt(Parser):
+    """Opt is useful for optional text"""
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            return result
+        else:
+            return Result(None, pos)
+
+
+class Rep(Parser):
+    """Rep applies its input parser repeatedly until it fails"""
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        results = []
+        result = self.parser(tokens, pos)
+        while result:
+            results.append(result.value)
+            pos = result.pos
+            result = self.parser(tokens, pos)
+
+        return Result(results, pos)
+
+
+class Process(Parser):
+    def __init__(self, parser, func):
+        self.parser = parser
+        self.func = func
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result:
+            result.value = self.func(result.value)
+            return result
+
+
+class Lazy(Parser):
+    def __init__(self, parser_func):
+        self.parser = None
+        self.parser_func = parser_func
+
+    def __call__(self, tokens, pos):
+        if not self.parser:
+            self.parser = self.parser_func()
+
+        return self.parser(tokens, pos)
+
+
+class Phrase(Parser):
+    """the top level parser for IMP"""
+    def __init__(self, parser):
+        self.parser = parser
+
+    def __call__(self, tokens, pos):
+        result = self.parser(tokens, pos)
+        if result and result.pos == len(tokens):
+            return result
         else:
             return None
