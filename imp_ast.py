5d179e14166f1782a339292c7e2d6326a56f1b0d
zhiruchen <zhiruchen1992@foxmail.com>
2017-04-09 16:45:50 +0800

    add eval method for statements

--- imp_ast.py
+++ imp_ast.py
@@ -17,6 +17,9 @@ class IntAexp(Aexp):
     def __init__(self, i):
         self.i = i
 
+    def eval(self, env):
+        return self.i
+
     def __repr__(self):
         return 'IntAexp({})'.format(self.i)
 
@@ -24,6 +27,9 @@ class IntAexp(Aexp):
 class VarAexp(Aexp):
     def __init__(self, name):
         self.name = name
+
+    def eval(self, env):
+        return env.get(self.name, 0)
 
     def __repr__(self):
         return 'VarExp({})'.format(self.name)
@@ -34,6 +40,20 @@ class BinopAexp(Aexp):
         self.op = op
         self.left = left
         self.right = right
+
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+        if self.op == '+':
+            return left_value + right_value
+        elif self.op == '-':
+            return left_value - right_value
+        elif self.op == '*':
+            return left_value * right_value
+        elif self.op == '/':
+            return left_value / right_value
+        else:
+            raise RuntimeError("unsupported operator: {}".format(self.op))
 
     def __repr__(self):
         return "BinopExp({0}, {1}, {2})".format(self.op, self.left, self.right)
@@ -49,7 +69,26 @@ class RelopBexp(Bexp):
         self.op = op
         self.left = left
         self.right = right
-        
+    
+    def eval(self, env):
+        left_value = self.left.eval(env)
+        right_value = self.right.eval(env)
+
+        if self.op == '<':
+            return left_value < right_value
+        elif self.op == '<=':
+            return left_value <= right_value
+        elif self.op == '>':
+            return left_value > right_value
+        elif self.op == '>=':
+            return left_value >= right_value
+        elif self.op == '=':
+            return left_value == right_value
+        elif self.op == '!=':
+            return left_value != right_value
+        else:
+            raise RuntimeError("unsupported operator: {}".format(self.op))
+
     def __repr__(self):
         return "BinopExp({0}, {1}, {2})".format(self.op, self.left, self.right)
 
@@ -58,7 +97,10 @@ class AndBexp(Bexp):
     def __init__(self, left, right):
         self.left = left
         self.right = right
-        
+    
+    def eval(self, env):
+        return self.left.eval(env) and self.right.eval(env)
+
     def __repr__(self):
         return "AndBexp({0}, {1})".format(self.left, self.right)
 
@@ -67,7 +109,10 @@ class OrBexp(Bexp):
     def __init__(self, left, right):
         self.left = left
         self.right = right
- 
+    
+    def eval(self, env):
+        return self.left.eval(env) or self.right.eval(env)
+
     def __repr__(self):
         return "OrBexp({0}, {1})".format(self.left, self.right)
 
@@ -75,6 +120,9 @@ class OrBexp(Bexp):
 class NotBexp(Bexp):
     def __init__(self, exp):
         self.exp = exp
+    
+    def eval(self, env):
+        return not self.exp.eval(env)
 
     def __repr__(self):
         return 'NotBexp({})'.format(self.exp)
@@ -88,6 +136,9 @@ class AssignStatement(Statement):
     def __init__(self, name, aexp):
         self.name = name
         self.aexp = aexp
+    
+    def eval(self, env):
+        env[self.name] = self.aexp.eval(env)
 
     def __repr__(self):
         return 'AssignStatement({},{})'.format(self.name, self.aexp)
@@ -97,6 +148,10 @@ class CompoundStatement(Statement):
     def __init__(self, first, second):
         self.first = first
         self.second = second
+
+    def eval(self, env):
+        self.first.eval(env)
+        self.second.eval(env)
 
     def __repr__(self):
         return 'CompoundStatement({},{})'.format(self.first, self.second)
@@ -108,6 +163,14 @@ class IfStatement(Statement):
         self.true_stmt = true_stmt
         self.false_stmt = false_stmt
 
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        if condition_value:
+            self.true_stmt.eval(env)
+        else:
+            if self.false_stmt:
+                self.false_stmt.eval(env)
+
     def __repr__(self):
         return 'IfStatement({},{},{})'.format(self.condition, self.true_stmt, self.false_stmt)
 
@@ -116,6 +179,12 @@ class WhileStatement(Statement):
     def __init__(self, condition, body):
         self.condition = condition
         self.body = body
+
+    def eval(self, env):
+        condition_value = self.condition.eval(env)
+        while condition_value:
+            self.body.eval(env)
+            condition_value = self.condition.eval(env)
 
     def __repr__(self):
         return 'WhileStatement({},{})'.format(self.condition, self.body)
